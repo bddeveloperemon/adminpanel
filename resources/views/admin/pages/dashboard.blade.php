a96c5fb34615025a2ab00af49f313e32fe480d09
unknown <Emon>
2024-08-01 01:49:58 +0600

    admin dashboard layout and admin login logout

---
+++ resources/views/admin/pages/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.layouts.master')
+
+@push('admin_style')
+@section('page_title', 'Dashboard')
+        
+@endpush
+
+@section('content')
+    <h1>Hello World</h1>
+@endsection
+
+@push('admin_script')
+    
+@endpush
